3a5a807de965ec25a255c617b73c642264c01c8d
Joseph Brant Wood <josephwood@Josephs-MacBook-Pro.local>
2018-01-11 11:39:35 -0500

    Modified code to create SEXP based off of mpz array as opposed to the bigvec from the R/gmp library

--- src/factorization.cc
+++ src/factorization.cc
@@ -17,14 +17,37 @@
 
 #include "Rgmp.h"
 #include "pollardrho.h"
+#include <stdio.h>
+#include <string.h>
 #include "factorization.h"
+
+unsigned long int intSize = sizeof(int); // starting with vector-size-header
+unsigned long int numb = 8*intSize;
+
+int myRaw(char* raw, mpz_t value) {
+    int totals = intSize;
+    
+    totals =  intSize * (2 + (mpz_sizeinbase(value,2)+numb-1) / numb);
+    memset(raw, 0, totals );
+    
+    int* r = (int*)raw;
+    r[0] = totals/intSize - 2;
+    
+    r[1] = (int) mpz_sgn(value);
+    mpz_export(&r[2], 0, 1, intSize, 0, 0, value);
+    
+    return totals;
+}
 
 SEXP getDivisorsC (SEXP n) {
     bigvec v = bigintegerR::create_bignum(n);
-    bigvec primeFacs, myFacs;
+    bigvec primeFacs;
+    
     if(v.size() > 0) {
         if (mpz_cmp_ui(v[0].value.getValueTemp(), 1) == 0) {
+            bigvec myFacs;
             myFacs.push_back(1);
+            return bigintegerR::create_SEXP(myFacs);
         } else {
             mpz_t val;
             mpz_init(val);
@@ -41,7 +64,8 @@ SEXP getDivisorsC (SEXP n) {
             getPrimeFactors (val,primeFacs);
             
             std::vector<int> lengths;
-            std::vector<biginteger>::iterator it;
+            std::vector<biginteger>::iterator it, primeEnd;
+            primeEnd = primeFacs.value.end();
             biginteger prev = primeFacs.value[0];
             
             unsigned long int i, j, k, n = primeFacs.size(), numUni = 0;
@@ -53,7 +77,7 @@ SEXP getDivisorsC (SEXP n) {
             lengths.push_back(1);
             k = 1;
             
-            for(it = primeFacs.value.begin() + 1; it < primeFacs.value.end(); it++) {
+            for(it = primeFacs.value.begin() + 1; it < primeEnd; it++) {
                 if (prev == *it) {
                     lengths[numUni]++;
                 } else {
@@ -68,13 +92,15 @@ SEXP getDivisorsC (SEXP n) {
             unsigned long int ind, facSize = 1, numFacs = 1;
             for (i = 0; i <= numUni; i++) {numFacs *= (lengths[i]+1);}
             
-            myFacs.value.reserve(numFacs);
-            mpz_t temp;
+            mpz_t myMPZ[numFacs];
+            for (i = 0; i < numFacs; i++) {mpz_init(myMPZ[i]);}
+            mpz_t temp, myPow;
             mpz_init(temp);
+            mpz_init(myPow);
             
             for (i = 0; i <= lengths[0]; ++i) {
                 mpz_pow_ui(temp, bigFacs[0], i);
-                myFacs.push_back(temp);
+                mpz_set(myMPZ[i], temp);
             }
             
             if (numUni > 0) {
@@ -82,17 +108,33 @@ SEXP getDivisorsC (SEXP n) {
                     facSize *= (lengths[j-1] + 1);
                     for (i = 1; i <= lengths[j]; i++) {
                         ind = i*facSize;
+                        mpz_pow_ui(myPow, bigFacs[j], i);
                         for (k = 0; k < facSize; k++) {
-                            mpz_pow_ui(temp, bigFacs[j], i);
-                            mpz_mul(temp, temp, myFacs[k].value.getValue());
-                            myFacs.push_back(temp);
+                            mpz_mul(temp, myPow, myMPZ[k]);
+                            mpz_set(myMPZ[ind + k], temp);
                         }
                     }
                 }
             }
             
-            std::sort(myFacs.value.begin(), myFacs.value.end());
+            unsigned long int size = intSize;
+            
+            for (i = 0; i < numFacs; ++i) // adding each bigint's needed size
+                size += intSize * (2 + (mpz_sizeinbase(myMPZ[i],2)+numb-1) / numb);
+                
+            SEXP ans = PROTECT(Rf_allocVector(RAWSXP, size));
+            char* r = (char*)(RAW(ans));
+            ((int*)(r))[0] = numFacs; // first int is vector-size-header
+            unsigned long int pos = intSize; // current position in r[] (starting after vector-size-header)
+            for (i = 0; i < numFacs; ++i)
+                pos += myRaw(&r[pos], myMPZ[i]);
+            
+            Rf_setAttrib(ans, R_ClassSymbol, Rf_mkString("bigz"));
+            UNPROTECT(1);
+            return(ans);
         }
     }
-    return bigintegerR::create_SEXP(myFacs);
 }
+
+// library(gmp)
+// test <- prod.bigz(2^14,3^5,7^4,nextprime(813274)^2,nextprime(2834)^4,13^5)
