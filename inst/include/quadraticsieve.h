0cb68fb229175c769eb847b57f4eb49e6a1584b9
Joseph Brant Wood <josephwood@Josephs-MacBook-Pro.local>
2018-01-11 11:40:06 -0500

    Updated commentary to explain variables and their actions.

--- inst/include/quadraticsieve.h
+++ inst/include/quadraticsieve.h
@@ -3,10 +3,15 @@
 #include "bigvec.h"
 
 /**
- * Factorize a prime number
+ * Factor large number into two smaller numbers (possibly prime)
  * t: number to factorize
+ * fudge1: number that alters the size of the prime factor base
+ * fudge2: number that adjusts the lower bound for the log sum
+ * LenB: Size of the sieving interval
  * factors [out]: the list of factors
  *
+ * If fudge1, fudge2, or LenB are left blank, they will be determined
+ * in the algorithm based off of the literature regarding the QS
  */
 
 void quadraticSieve (mpz_t myNum, double fudge1,
