3ba5a841b3e0fca282c9e15a4c650a047aca2d4c
Joseph Brant Wood <josephwood@Josephs-MacBook-Pro.local>
2018-03-15 21:52:38 -0400

    Changed name of function

--- R/biginteger.R
+++ R/biginteger.R
@@ -1,4 +1,4 @@
-factorizeAllBig <- function(n) .Call("getDivisorsC", n, PACKAGE = "bigIntegerAlgos")
+divisorsBig <- function(v, namedList = FALSE) .Call("getDivisorsC", v, namedList, PACKAGE = "bigIntegerAlgos")
 
 quadraticSieve <- function(n) .Call("QuadraticSieveContainer", n, PACKAGE = "bigIntegerAlgos")
 
