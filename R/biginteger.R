52310d73b924d8bcb1744f97f6e2396d47467380
Joseph Brant Wood <josephwood@Josephs-MacBook-Pro.local>
2018-03-20 22:00:52 -0400

    Removed files associated with bigvec classes. Now only using mpz_t arrays

--- R/biginteger.R
+++
@@ -1,5 +0,0 @@
-divisorsBig <- function(v, namedList = FALSE) .Call("getDivisorsC", v, namedList, PACKAGE = "bigIntegerAlgos")
-
-quadraticSieve <- function(n) .Call("QuadraticSieveContainer", n, PACKAGE = "bigIntegerAlgos")
-
-quadResSquareRoot <- function(n, p) .Call("QuadraticResidueContainer", n, p, PACKAGE = "bigIntegerAlgos")
