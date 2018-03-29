d77b166b2130d2b5c0ba36e90edebdb67db389fa
Joseph Brant Wood <josephwood@Josephs-MacBook-Pro.local>
2018-03-28 20:56:44 -0400

    Added quicksort

--- inst/include/importExportMPZ.h
+++ inst/include/importExportMPZ.h
@@ -1,6 +1,7 @@
 #ifndef IMPORTEXPORTMPZ_GMP_R
 #define IMPORTEXPORTMPZ_GMP_R 1
 
+#include <vector>
 #include "Rgmp.h"
 
 typedef void (*gmp_binary)(mpz_t, const mpz_t, const mpz_t);
@@ -12,5 +13,7 @@ typedef void (*gmp_binary)(mpz_t, const mpz_t, const mpz_t);
 void createMPZArray (SEXP v, mpz_t myVec[], unsigned int sizevec);
 
 int myRaw (char* raw, mpz_t value, unsigned long int totals);
+
+void quickSort(mpz_t arr[], int left, int right, std::vector<unsigned int>& lens);
 
 #endif
