3c1c2c71bad7ead5db22afc9894c229258ac4057
Joseph Brant Wood <jwood000@gmail.com>
2020-09-22 21:21:53 -0400

    Updated headers for SieveUtils and Polynomial

--- inst/include/Polynomial.h
+++ inst/include/Polynomial.h
@@ -12,37 +12,32 @@ private:
     vec2dint powsOfSmooths;
     vec2dint powsOfPartials;
     
-    std::vector<std::size_t> coFactorIndexVec;
     std::vector<int> myStart;
     
     hash64vec partFactorsMap;
     hash64mpz partIntvlMap;
-    hash64size_t keepingTrack;
     
     std::vector<mpz_class> smoothInterval;
-    std::vector<double> largeCoFactors;
+    std::vector<uint64_t> largeCoFactors;
     std::vector<mpz_class> partialInterval;
     
     std::size_t nPolys;
     std::size_t nPartial;
     std::size_t nSmooth;
-    std::size_t coFactorInd;
     int mpzFacSize;
     
+    bool GoSingle;
     std::size_t SaPThresh;
     const std::size_t facSize;
     const bool bShowStats;
     
     void MergeMaster(vec2dint &powsOfSmoothsBig, vec2dint &powsOfPartialsBig,
-                     std::vector<std::size_t> &coFactorIndexVecBig,
                      hash64vec &partFactorsMapBig, hash64mpz &partIntvlMapBig,
-                     hash64size_t &keepingTrackBig, std::vector<mpz_class> &smoothIntervalBig,
-                     std::vector<double> &largeCoFactorsBig, 
+                     std::vector<mpz_class> &smoothIntervalBig,
+                     std::vector<uint64_t> &largeCoFactorsBig, 
                      std::vector<mpz_class> &partialIntervalBig);
     
     void SetMpzFacSize(int _mpzFacSize) {mpzFacSize = _mpzFacSize;}
-    
-    std::size_t GetSmoothSize() const {return smoothInterval.size();};
     
     void SievePolys(const std::vector<std::size_t> &SieveDist,
                     const std::vector<int> &facBase, const std::vector<int> &LnFB, 
@@ -53,21 +48,8 @@ private:
     
 public:
     
-    // Polynomial(Polynomial const&) = delete;
-    // Polynomial& operator=(Polynomial const&) = delete;
-    // Polynomial(Polynomial&&) = delete;
-    // //! copy constructor (forbidden)
-    // RMonitor(RMonitor const&) = delete;
-    // //! move constructor (forbidden)
-    // RMonitor(RMonitor&&) = delete;
-    // //! copy assignment (forbidden)
-    // RMonitor& operator=(RMonitor const&) = delete;
-    // //! move assignment (forbidden)
-    // RMonitor& operator=(RMonitor &&) = delete;
-    
     Polynomial(std::size_t _facSize);
     
-    // SaPThresh: Smooth + Partial Threshold
     Polynomial(std::size_t _mpzContainerSize,
                std::size_t _facSize, bool _bShowStats, const mpz_class &myNum);
     
--- inst/include/SieveUtils.h
+++ inst/include/SieveUtils.h
@@ -15,9 +15,9 @@ constexpr int L1Cache = 32768;
 using vec2dint = std::vector<std::vector<int>>;
 using hash64vec = std::unordered_map<std::uint64_t, std::vector<int>>;
 using hash64mpz = std::unordered_map<std::uint64_t, mpz_class>;
-using hash64size_t = std::unordered_map<std::uint64_t, std::size_t>;
 
-std::vector<std::size_t> SetSieveDist(const std::vector<int> &facBase, const mpz_class &myNum);
+std::vector<std::size_t> SetSieveDist(const std::vector<int> &facBase,
+                                      const mpz_class &myNum);
 
 std::vector<std::uint8_t> MyIntToBit(std::size_t x, std::size_t dig);
 
@@ -27,12 +27,13 @@ std::vector<int> GetPrimesQuadRes(const mpz_class &myN, double LimB, double fudg
 void SinglePoly(const std::vector<std::size_t> &SieveDist,
                 const std::vector<int> &facBase, const std::vector<int> &LnFB,
                 vec2dint &powsOfSmooths, vec2dint &powsOfPartials,
-                std::vector<std::size_t> &coFactorIndexVec, std::vector<int> &myStart,
-                hash64vec &partFactorsMap, hash64mpz &partIntvlMap, hash64size_t &keepingTrack,
-                std::vector<mpz_class> &smoothInterval, std::vector<double> &largeCoFactors,
-                std::vector<mpz_class> &partialInterval, const mpz_class &NextPrime,
-                const mpz_class &LowBound, const mpz_class &myNum, std::size_t &nPartial,
-                std::size_t &nSmooth, std::size_t &coFactorInd, int theCut,
-                int DoubleLenB, int mpzFacSize, int vecMaxSize, std::size_t strt);
+                std::vector<int> &myStart, hash64vec &partFactorsMap,
+                hash64mpz &partIntvlMap, std::vector<mpz_class> &smoothInterval,
+                std::vector<uint64_t> &largeCoFactors,
+                std::vector<mpz_class> &partialInterval,
+                const mpz_class &NextPrime, const mpz_class &LowBound,
+                const mpz_class &myNum, std::size_t &nPartial,
+                std::size_t &nSmooth, int theCut,int DoubleLenB,
+                int mpzFacSize, int vecMaxSize, std::size_t strt);
 
 #endif
