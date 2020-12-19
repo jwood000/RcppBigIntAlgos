8cb899e85398e19e369b473a9b17c687805a274a
Joseph Brant Wood <jwood000@gmail.com>
2020-12-19 13:20:36 -0500

    Increased curve threshold

--- inst/include/LenstraECM.h
+++ inst/include/LenstraECM.h
@@ -9,10 +9,11 @@ constexpr unsigned long int MaxCurveBound = std::numeric_limits<unsigned long in
 
 static const std::map<unsigned long int, int> CurveLookup = {{50, 8}, {55, 9},
                                                              {60, 10}, {65, 11},
-                                                             {70, 12}, {77, 13},
-                                                             {84, 14}, {91, 15},
-                                                             {100, 16}, 
-                                                             {MaxCurveBound, 17}};
+                                                             {70, 12}, {75, 13},
+                                                             {80, 14}, {85, 15},
+                                                             {90, 16}, {95, 17},
+                                                             {100, 18},
+                                                             {MaxCurveBound, 19}};
 
 unsigned long int GetMaxCurves(std::size_t maxLoopIter);
 std::vector<unsigned long int> GenerateNPrimes(std::size_t limit);
