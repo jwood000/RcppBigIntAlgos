d9506dfb0b7a4ee490023a936ebacb0b22b9f31c
Joseph Wood <jwood000@gmail.com>
2023-07-15 12:56:43 -0400

    Deleted several files for new update. Now using cpp11, system gmpxx headers, github actions, and at least C++14.

--- inst/include/CleanConvert.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLEAN_CONVERT_H
-#define CLEAN_CONVERT_H
-
-#include <Rcpp.h>
-
-bool convertLogical(SEXP boolInput, const std::string &nameOfBool);
-void convertInt(SEXP input, int &result, const std::string &nameOfObject);
-
-#endif
